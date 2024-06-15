34a3c047c762c8e4eb45d0470f4167a74ca52f7a
Mohammed Qureshi <mohammedqureshi2998@gmail.com>
2024-06-15 12:25:00 +0100

    Fix menu

--- Menu/MainMenu/MainMenuScript.gd
+++ Menu/MainMenu/MainMenuScript.gd
@@ -7,7 +7,7 @@ func _process(delta):
 	pass
 
 func _on_new_game_pressed():
-	get_tree().change_scene_to_file("res://scenes/Game.tscn")
+	get_tree().change_scene_to_file("res://Game/Game.tscn")
 	
 func _on_exit_pressed():
 	get_tree().quit()
--- Menu/PauseMenu/PauseMenuScript.gd
+++ Menu/PauseMenu/PauseMenuScript.gd
@@ -6,7 +6,7 @@ func _on_resume_pressed():
 	game.pause(true)
 
 func _on_main_menu_pressed():
-	get_tree().change_scene_to_file("res://MainMenu/MainMenuScene.tscn");
+	get_tree().change_scene_to_file("res://Menu/MainMenu/MainMenuScene.tscn");
 
 func _on_quit_pressed():
 	get_tree().quit();
