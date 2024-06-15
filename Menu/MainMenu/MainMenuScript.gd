745c0bf180c10cfbf540cda2e45474f096460b66
Mohammed Qureshi <mohammedqureshi2998@gmail.com>
2024-06-15 23:55:22 +0100

    Clean up and variable names

--- Menu/MainMenu/MainMenuScript.gd
+++ Menu/MainMenu/MainMenuScript.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const GAME_SCENE_PATH = "res://Game/Game.tscn";
+
 func _ready():
 	get_tree().paused = false;
 
@@ -7,7 +9,7 @@ func _process(delta):
 	pass
 
 func _on_new_game_pressed():
-	get_tree().change_scene_to_file("res://Game/Game.tscn")
+	get_tree().change_scene_to_file(GAME_SCENE_PATH)
 	
 func _on_exit_pressed():
 	get_tree().quit()
--- Menu/PauseMenu/PauseMenuScript.gd
+++ Menu/PauseMenu/PauseMenuScript.gd
@@ -2,11 +2,13 @@ extends Control
 
 @onready var game = $"../../.."
 
+const MAIN_MENU_SCENE_PATH = "res://Menu/MainMenu/MainMenuScene.tscn"
+
 func _on_resume_pressed():
 	game.pause(true)
 
 func _on_main_menu_pressed():
-	get_tree().change_scene_to_file("res://Menu/MainMenu/MainMenuScene.tscn");
+	get_tree().change_scene_to_file(MAIN_MENU_SCENE_PATH);
 
 func _on_quit_pressed():
 	get_tree().quit();
